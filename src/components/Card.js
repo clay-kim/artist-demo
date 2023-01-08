5a95aba97e92f5eff63d125e0d1726d9e9d40733
Sung Kim <clay1013@gmail.com>
2023-01-07 20:52:04 -0800

    art update/available font

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Store from '../pages/Store';
 import './Card.css';
-import { Link } from 'react-router-dom';
 
 function Card(props) {
 
--- src/components/CardArt.js
+++ src/components/CardArt.js
@@ -1,12 +1,9 @@
 import React, { useState } from 'react'
 import './CardArt.css';
-import { Link, useParams } from 'react-router-dom';
-
 
 function CardArt(props) {
   console.log('This is == CardArt ===');
 
-    
   return (
 
     <div class="cardArt">
